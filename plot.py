f542d6c4e5e24ce52609fc8eb388eb154f638e56
David M. Rogers <predictivestatmech@gmail.com>
2020-07-17 18:21:59 -0400

    Fixes to plot.py syntax.

--- plot.py
+++ plot.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pylab as plt
 
+#min_n = 50
+min_n = 1
+
 z = np.load("categories.npy").astype(int)
 rmsd = np.load("../rmsdmat.npy")
 if len(z)//10 == len(rmsd): # hack due to our particular trj.
@@ -35,30 +38,33 @@ def permute(rmsd, z, K):
 
     return x, Nk
 
-def plot_hist(c, n, ax, **args):
-    dc = bins[2]-bins[1]
-    c = 0.5*(bins[1:]+bins[:-1])
-    ax.plot(c, n.astype(float)/(dc*n.sum()), **args)
+def plot_hist(c, n, ax, *args, **kws):
+    dc = c[2]-c[1]
+    ax.plot(c, n.astype(float)/(dc*n.sum()), *args, **kws)
 
 def plt_pairs(rmsd, Nk, ax):
     nb, bins = np.histogram(rmsd.reshape(-1), 100)
+    c = 0.5*(bins[1:]+bins[:-1])
     bins[0] += 0.001
 
     u = 0
     for i in range(K):
-        if Nk[i] == 0: continue
-        v = np.sum(Nk[:i+1])
-        for j in range(i+1,K):
-            if Nk[j] == 0: continue
-            n, _ = np.histogram(rmsd[u:u+Nk[i],v:v+Nk[j]].reshape(-1), bins)
-            plot_hist(c, b, ax, 'r-')
-            v += Nk[j]
+        if Nk[i] >= min_n:
+            v = np.sum(Nk[:i+1])
+            for j in range(i+1,K):
+                if Nk[j] >= min_n:
+                    n, _ = np.histogram(rmsd[u:u+Nk[i],v:v+Nk[j]].reshape(-1), bins)
+                    plot_hist(c, n, ax, 'r-')
+                v += Nk[j]
         u += Nk[i]
     u = 0
     for i in range(K):
-        if Nk[i] == 0: continue
-        n, _ = np.histogram(rmsd[u:u+Nk[i],u:u+Nk[i]].reshape(-1), bins)
-        plot_hist(c, b, ax, 'k-')
+        if Nk[i] >= min_n:
+            n, _ = np.histogram(rmsd[u:u+Nk[i],u:u+Nk[i]].reshape(-1), bins)
+            color = 'k-'
+            #if i == 2:
+            #    color = 'g-'
+            plot_hist(c, n, ax, color)
         u += Nk[i]
 
     plot_hist(c, nb, ax, 'b-', linewidth=1.5)
@@ -79,6 +85,7 @@ fig, ax = plt.subplots(1, 2, figsize=(6.5,4))
 x, Nk = permute(rmsd, z, K)
 
 plt_pairs(x, Nk, ax[0])
+ax[0].set_xlim(1.5, 5.5)
 plt_hist(x, Nk, ax[1])
 plt.savefig("rmsd.png")
 plt.show()
